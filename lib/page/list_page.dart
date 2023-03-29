73a6a7bd77179b3792d52294218048eaca30148e
Antoinebac435 <antoinebacquet62@gmail.com>
2023-03-29 15:10:49 +0200

    List page
    Username + Enter

--- lib/page/list_page.dart
+++ lib/page/list_page.dart
@@ -1,135 +1,185 @@
 // ignore_for_file: prefer_const_constructors, directives_ordering
 
 import 'package:flutter/material.dart';
-import 'package:tekartik_common_utils/common_utils_import.dart';
-import 'package:tekartik_notepad_sqflite_app/main.dart';
-import 'package:tekartik_notepad_sqflite_app/model/model.dart';
-import 'package:tekartik_notepad_sqflite_app/page/edit_page.dart';
-import 'package:tekartik_notepad_sqflite_app/page/note_page.dart';
+// import 'package:tekartik_common_utils/common_utils_import.dart';
+// import 'package:tekartik_notepad_sqflite_app/main.dart';
+// import 'package:tekartik_notepad_sqflite_app/model/model.dart';
+// import 'package:tekartik_notepad_sqflite_app/page/edit_page.dart';
+// import 'package:tekartik_notepad_sqflite_app/page/note_page.dart';
 
-class NoteListPage extends StatefulWidget {
-  const NoteListPage({Key? key}) : super(key: key);
+
+
+
+class UsernameInput extends StatefulWidget {
+  const UsernameInput({super.key});
 
   @override
   // ignore: library_private_types_in_public_api
-  _NoteListPageState createState() => _NoteListPageState();
+  _UsernameInputState createState() => _UsernameInputState();
 }
 
-class _NoteListPageState extends State<NoteListPage> {
-  Widget _deleteSingleContactDialog(BuildContext context, DbNote note) {
-    return AlertDialog(
-      icon: Icon(Icons.warning),
-      title: Text('Delete contact'),
-      content: Text('Are you sure you want to delete this contact ?'),
-      actions: [
-        TextButton(
-          child: Text('CANCEL'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        TextButton(
-          child: Text('DELETE'),
-          onPressed: () {
-            noteProvider.deleteNote(note.id.v!);
-            Navigator.of(context).pop();
-          },
-        ),
-      ],
-    );
-  }
-
-  Widget _deleteContactsDialog(BuildContext context) {
-    return AlertDialog(
-      icon: Icon(Icons.warning),
-      title: Text('Delete all contacts'),
-      content: Text('Are you sure you want to delete all your contacts ?'),
-      actions: [
-        TextButton(
-          child: Text('CANCEL'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        TextButton(
-          child: Text('DELETE'),
-          onPressed: () {
-            noteProvider.clearAllNotes();
-            Navigator.of(context).pop();
-          },
-        ),
-      ],
-    );
-  }
+class _UsernameInputState extends State<UsernameInput> {
+  final _usernameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Address Book',
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        TextField(
+          controller: _usernameController,
+          decoration: InputDecoration(
+            labelText: 'Nom d\'utilisateur',
+            border: OutlineInputBorder(),
+          ),
+          onSubmitted: (_) => _submitUsername(),
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.delete),
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return _deleteContactsDialog(context);
-                },
-              );
-            },
-          )
-        ],
-      ),
-      body: StreamBuilder<List<DbNote?>>(
-        stream: noteProvider.onNotes(),
-        builder: (context, snapshot) {
-          var notes = snapshot.data;
-          if (notes == null) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          return ListView.builder(
-              itemCount: notes.length,
-              itemBuilder: (context, index) {
-                var note = notes[index]!;
-                return ListTile(
-                  title: Text(
-                      '${note.noteLastName.v ?? ''} ${note.noteFirstName.v ?? ''}'),
-                  subtitle: note.noteFirstName.v?.isNotEmpty ?? false
-                      ? Text(LineSplitter.split(note.notePhone.v!).first)
-                      : null,
-                  onTap: () {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) {
-                      return NotePage(
-                        noteId: note.id.v,
-                      );
-                    }));
-                  },
-                  onLongPress: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return _deleteSingleContactDialog(context, note);
-                      },
-                    );
-                  },
-                );
-              });
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-            return EditNotePage(initialNote: null);
-          }));
-        },
-        child: Icon(Icons.add),
-      ),
+        SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: _submitUsername,
+          child: Text('Entrer'),
+        ),
+      ],
     );
   }
+
+  void _submitUsername() {
+    final username = _usernameController.text.trim();
+    if (username.isNotEmpty) {
+      // Do something with the username
+    }
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    super.dispose();
+  }
 }
+
+// class NoteListPage extends StatefulWidget {
+//   const NoteListPage({Key? key}) : super(key: key);
+
+//   @override
+//   // ignore: library_private_types_in_public_api
+//   _NoteListPageState createState() => _NoteListPageState();
+// }
+
+// class _NoteListPageState extends State<NoteListPage> {
+//   Widget _deleteSingleContactDialog(BuildContext context, DbNote note) {
+//     return AlertDialog(
+//       icon: Icon(Icons.warning),
+//       title: Text('Delete contact'),
+//       content: Text('Are you sure you want to delete this contact ?'),
+//       actions: [
+//         TextButton(
+//           child: Text('CANCEL'),
+//           onPressed: () {
+//             Navigator.of(context).pop();
+//           },
+//         ),
+//         TextButton(
+//           child: Text('DELETE'),
+//           onPressed: () {
+//             noteProvider.deleteNote(note.id.v!);
+//             Navigator.of(context).pop();
+//           },
+//         ),
+//       ],
+//     );
+//   }
+
+//   Widget _deleteContactsDialog(BuildContext context) {
+//     return AlertDialog(
+//       icon: Icon(Icons.warning),
+//       title: Text('Delete all contacts'),
+//       content: Text('Are you sure you want to delete all your contacts ?'),
+//       actions: [
+//         TextButton(
+//           child: Text('CANCEL'),
+//           onPressed: () {
+//             Navigator.of(context).pop();
+//           },
+//         ),
+//         TextButton(
+//           child: Text('DELETE'),
+//           onPressed: () {
+//             noteProvider.clearAllNotes();
+//             Navigator.of(context).pop();
+//           },
+//         ),
+//       ],
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           'Address Book',
+//         ),
+//         actions: [
+//           IconButton(
+//             icon: Icon(Icons.delete),
+//             onPressed: () {
+//               showDialog(
+//                 context: context,
+//                 builder: (context) {
+//                   return _deleteContactsDialog(context);
+//                 },
+//               );
+//             },
+//           )
+//         ],
+//       ),
+//       body: StreamBuilder<List<DbNote?>>(
+//         stream: noteProvider.onNotes(),
+//         builder: (context, snapshot) {
+//           var notes = snapshot.data;
+//           if (notes == null) {
+//             return Center(
+//               child: CircularProgressIndicator(),
+//             );
+//           }
+//           return ListView.builder(
+//               itemCount: notes.length,
+//               itemBuilder: (context, index) {
+//                 var note = notes[index]!;
+//                 return ListTile(
+//                   title: Text(
+//                       '${note.noteLastName.v ?? ''} ${note.noteFirstName.v ?? ''}'),
+//                   subtitle: note.noteFirstName.v?.isNotEmpty ?? false
+//                       ? Text(LineSplitter.split(note.notePhone.v!).first)
+//                       : null,
+//                   onTap: () {
+//                     Navigator.of(context)
+//                         .push(MaterialPageRoute(builder: (context) {
+//                       return NotePage(
+//                         noteId: note.id.v,
+//                       );
+//                     }));
+//                   },
+//                   onLongPress: () {
+//                     showDialog(
+//                       context: context,
+//                       builder: (context) {
+//                         return _deleteSingleContactDialog(context, note);
+//                       },
+//                     );
+//                   },
+//                 );
+//               });
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+//             return EditNotePage(initialNote: null);
+//           }));
+//         },
+//         child: Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
