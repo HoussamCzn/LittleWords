9bc64d09990a0da07b08b3f1907c2fff661e1bb1
HoussamCzn <105058712+HoussamCzn@users.noreply.github.com>
2023-04-10 10:34:15 +0200

    refactor: remove imports and comments
    Comments consisted of old code, now unnecessary so removed.

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -2,19 +2,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:tekartik_notepad_sqflite_app/page/menu_Page.dart';
-// import 'package:tekartik_common_utils/common_utils_import.dart';
-// import 'package:tekartik_notepad_sqflite_app/main.dart';
-// import 'package:tekartik_notepad_sqflite_app/model/model.dart';
-// import 'package:tekartik_notepad_sqflite_app/page/edit_page.dart';
-// import 'package:tekartik_notepad_sqflite_app/page/note_page.dart';
+import '../page/map_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
-  _LoginPageState createState() => _LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
@@ -47,7 +41,7 @@ class _LoginPageState extends State<LoginPage> {
                     if (_formKey.currentState!.validate()) {
                       _submitUsername();
                       Navigator.of(context).pushReplacement(MaterialPageRoute(
-                          builder: (context) => const MenuPage()));
+                          builder: (context) => const MapPage()));
                     }
                   },
                   child: const Text('Submit'),
@@ -70,130 +64,3 @@ class _LoginPageState extends State<LoginPage> {
     super.dispose();
   }
 }
-
-// class NoteListPage extends StatefulWidget {
-//   const NoteListPage({Key? key}) : super(key: key);
-
-//   @override
-//   // ignore: library_private_types_in_public_api
-//   _NoteListPageState createState() => _NoteListPageState();
-// }
-
-// class _NoteListPageState extends State<NoteListPage> {
-//   Widget _deleteSingleContactDialog(BuildContext context, DbNote note) {
-//     return AlertDialog(
-//       icon: Icon(Icons.warning),
-//       title: Text('Delete contact'),
-//       content: Text('Are you sure you want to delete this contact ?'),
-//       actions: [
-//         TextButton(
-//           child: Text('CANCEL'),
-//           onPressed: () {
-//             Navigator.of(context).pop();
-//           },
-//         ),
-//         TextButton(
-//           child: Text('DELETE'),
-//           onPressed: () {
-//             noteProvider.deleteNote(note.id.v!);
-//             Navigator.of(context).pop();
-//           },
-//         ),
-//       ],
-//     );
-//   }
-
-//   Widget _deleteContactsDialog(BuildContext context) {
-//     return AlertDialog(
-//       icon: Icon(Icons.warning),
-//       title: Text('Delete all contacts'),
-//       content: Text('Are you sure you want to delete all your contacts ?'),
-//       actions: [
-//         TextButton(
-//           child: Text('CANCEL'),
-//           onPressed: () {
-//             Navigator.of(context).pop();
-//           },
-//         ),
-//         TextButton(
-//           child: Text('DELETE'),
-//           onPressed: () {
-//             noteProvider.clearAllNotes();
-//             Navigator.of(context).pop();
-//           },
-//         ),
-//       ],
-//     );
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(
-//           'Address Book',
-//         ),
-//         actions: [
-//           IconButton(
-//             icon: Icon(Icons.delete),
-//             onPressed: () {
-//               showDialog(
-//                 context: context,
-//                 builder: (context) {
-//                   return _deleteContactsDialog(context);
-//                 },
-//               );
-//             },
-//           )
-//         ],
-//       ),
-//       body: StreamBuilder<List<DbNote?>>(
-//         stream: noteProvider.onNotes(),
-//         builder: (context, snapshot) {
-//           var notes = snapshot.data;
-//           if (notes == null) {
-//             return Center(
-//               child: CircularProgressIndicator(),
-//             );
-//           }
-//           return ListView.builder(
-//               itemCount: notes.length,
-//               itemBuilder: (context, index) {
-//                 var note = notes[index]!;
-//                 return ListTile(
-//                   title: Text(
-//                       '${note.noteLastName.v ?? ''} ${note.noteFirstName.v ?? ''}'),
-//                   subtitle: note.noteFirstName.v?.isNotEmpty ?? false
-//                       ? Text(LineSplitter.split(note.notePhone.v!).first)
-//                       : null,
-//                   onTap: () {
-//                     Navigator.of(context)
-//                         .push(MaterialPageRoute(builder: (context) {
-//                       return NotePage(
-//                         noteId: note.id.v,
-//                       );
-//                     }));
-//                   },
-//                   onLongPress: () {
-//                     showDialog(
-//                       context: context,
-//                       builder: (context) {
-//                         return _deleteSingleContactDialog(context, note);
-//                       },
-//                     );
-//                   },
-//                 );
-//               });
-//         },
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () {
-//           Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-//             return EditNotePage(initialNote: null);
-//           }));
-//         },
-//         child: Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
