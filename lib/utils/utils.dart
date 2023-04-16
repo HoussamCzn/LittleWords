6f2a16f067a95ee2f5458a9d729fddb53cc5d7bd
HoussamCzn <105058712+HoussamCzn@users.noreply.github.com>
2023-04-17 00:11:52 +0200

    refactor: add correct return types to functions
    Added Future<type> instead of void to handle better the cases of failures.

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_build_context_synchronously, avoid_dynamic_calls
+// ignore_for_file: avoid_dynamic_calls
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,7 +11,7 @@ import '../provider/device_location.provider.dart';
 import '../provider/dio.provider.dart';
 import '../provider/words_around.provider.dart';
 
-void saveWordToDatabase(
+Future<void> saveWordToDatabase(
     BuildContext context, WidgetRef ref, WordDTO word) async {
   final dio = ref.read(dioProvider);
   final url =
@@ -23,22 +23,20 @@ void saveWordToDatabase(
       return response.data['data'];
     }
   });
+
   final note = DbNote()
     ..usernameField.value = res['author'].toString()
     ..wordField.value = res['content'].toString()
     ..dateField.value = DateTime.now().millisecondsSinceEpoch;
 
   await noteProvider.saveNote(note);
-  const snackBar = SnackBar(
-    content: Text('Word added to the database'),
-  );
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
 
   ref.invalidate(deviceLocationProvider);
   ref.invalidate(wordsAroundProvider);
 }
 
-void submitWord(BuildContext context, WidgetRef ref, String text) async {
+Future<bool> submitWord(
+    BuildContext context, WidgetRef ref, String text) async {
   final prefs = await SharedPreferences.getInstance();
   final username = prefs.get('username').toString();
   final dio = ref.read(dioProvider);
@@ -56,24 +54,11 @@ void submitWord(BuildContext context, WidgetRef ref, String text) async {
     final word =
         WordDTO(null, username, text, location.latitude, location.longitude);
     await dio.post(url, data: word.toJson());
-    const snackbar = SnackBar(
-      content: Text('Word added !'),
-    );
-    ScaffoldMessenger.of(context).showSnackBar(snackbar);
-  } else {
-    final snackbar = SnackBar(
-      content: const Text('Location not available'),
-      action: SnackBarAction(
-        label: 'Retry',
-        onPressed: () {
-          submitWord(context, ref, text);
-        },
-      ),
-    );
-    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+
+    return true;
   }
 
-  Navigator.pop(context);
+  return false;
 }
 
 String logTruncate(String text) {
